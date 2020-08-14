5bd9fdc499729167de4a906cd4ff51cace674a5f
Mohamed Goui <goui.mohamed01@gmail.com>
2020-08-14 14:12:36 +0100

    making Profile component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Profile from './profile/Profile';
 
 function App() {
+  const imgStyle = {
+    width: 300,
+    height: 350,
+    borderRadius: "15px 50px"
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Profile firstName = "Tom Hardy" bio="Tom Hardy, in full Edward Thomas Hardy, (born September 15, 1977, London, England), British actor who was known for his striking good looks, idiosyncratic personality, and cerebral performances in both cult films and mainstream blockbusters." profession="Actor & Producer">
+      <div>
+      <img src="/tom.jpg" alt="Tom Hardy Smiles" style={imgStyle}/>
+      </div>
+    </Profile>
+    <Profile firstName = "Lionel Messi" bio="Lionel Andrés Messi Cuccittini is an Argentine professional footballer who plays as a forward and captains both Spanish club Barcelona and the Argentina national team." profession="Football Player">
+    <div>
+    <img src="/messi.jpg" alt="Lionel Messi with Argentina kit" style={imgStyle}/>
     </div>
+  </Profile>
+  </>
   );
 }
 
---
+++ src/profile/Profile.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Profile = (props) => {
+    const { firstName = "Anonymous", bio = "Anonymous", profession = "Anonymous" } = props;
+    const handleName = () => {
+        alert(`${firstName}
+        ${bio}
+        ${profession}
+        `);
+    }
+    const styleDiv = {
+        textAlign: "center",
+        borderLeft: "6px solid red",
+        backgroundColor: "lightgrey",
+        borderRadius: 300,
+        borderRight: "6px solid blue",
+        marginBottom: 50 
+    }
+    const paragraphStyle = {
+        width: 300,
+        marginLeft: "40%"
+    }
+    const buttonStyle = {
+        padding: "20px 20px",
+        color:"red",
+        fontSize: 15,
+        borderRadius: 25
+    }
+    return (
+    <div style={styleDiv}>
+    <h1 style={{color:"brown",letterSpacing:10, fontFamily:"cursive"}}>{firstName}</h1>
+    <p style={paragraphStyle}>{bio}</p>
+    <h3 style={{color:"brown", fontFamily:"fantasy"}}>{profession}</h3>
+    {props.children}
+    <button style={buttonStyle} onClick={handleName}>Show Name</button>
+    </div>  
+    );
+    
+}
+
+export default Profile;
